21590ddc63800f0b7deec14dc514ea37fc6750df
akcorp2003 <kuanga2016@ucla.edu>
2016-02-25 23:05:28 -0800

    Added Files
    Added files to implementation.

---
+++ src/edu/ucla/cs/cs144/Item.java
@@ -0,0 +1,24 @@
+package edu.ucla.cs.cs144;
+
+import java.util.ArrayList;
+
+public class Item {
+	public String item_id;
+	public String name;
+	public String buy_price;
+	public String first_bid;
+	public String num_bids;
+	public String seller_id;
+	public String seller_rating;
+	public String started;
+	public String ends;
+	public String desc;
+	public ArrayList<String> categories;
+	public ArrayList<Bid> bids;
+	
+	public Item(){
+		
+	}
+	
+	
+}
---
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -0,0 +1,59 @@
+package edu.ucla.cs.cs144;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+
+public class ItemServlet extends HttpServlet implements Servlet {
+       
+    public ItemServlet() {}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        // your codes here
+    	try{
+    		String id = request.getParameter("id");
+    		/*if(id == null || id == ""){
+    			return;
+    		}*/
+    		request.setAttribute("test", "hellothis is a test");
+    		String itemxml = AuctionSearchClient.getXMLDataForItemId(id);
+    		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    		DocumentBuilder builder = factory.newDocumentBuilder();
+   			InputSource is = new InputSource(new StringReader(itemxml));
+    		Document doc = builder.parse(is);
+    		
+    		Item item = new Item();
+    		item.item_id = id;
+    		item.name = doc.getElementsByTagName("Name").item(0).getTextContent();
+    		if(doc.getElementsByTagName("Buy_Price").item(0) != null) {
+				item.buy_price = doc.getElementsByTagName("Buy_Price").item(0).getTextContent();
+			}
+			else{
+				item.buy_price = "N/A"; //to avoid warnings on MySQL
+			}
+    		item.first_bid = doc.getElementsByTagName("First_Bid").item(0).getTextContent();
+    		item.num_bids = doc.getElementsByTagName("Number_of_Bids").item(0).getTextContent();
+    		Element n_userid = (Element) doc.getElementsByTagName("Seller").item(0);
+			item.seller_id = n_userid.getAttribute("UserID");
+    		item.seller_rating = n_userid.getAttribute("Rating");
+    		
+    		request.setAttribute("item", item);
+    		request.getRequestDispatcher("/item.jsp").forward(request, response);
+    		
+    	} catch(Exception e){
+    		
+    	}
+    }
+}
